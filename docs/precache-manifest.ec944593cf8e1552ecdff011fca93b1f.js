b860b6ba9a4bfa92772036f5728d7d906f5a5aaa
한광재 <hkj4900@pusan.ac.kr>
2021-05-08 12:37:03 +0900

    [release]

---
+++ docs/precache-manifest.ec944593cf8e1552ecdff011fca93b1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602f5b76d2fbbf5e03d4",
+    "url": "/portfolio/css/app.9ec0eb38.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/portfolio/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e9dc9f5d3770fe241c2451fba6222497",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a37712192687ff4c6c02",
+    "url": "/portfolio/js/about.90cabc3b.js"
+  },
+  {
+    "revision": "602f5b76d2fbbf5e03d4",
+    "url": "/portfolio/js/app.cc9706dc.js"
+  },
+  {
+    "revision": "3124973107d35d258d0d",
+    "url": "/portfolio/js/chunk-vendors.42b38e9a.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/portfolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio/robots.txt"
+  }
+]);
